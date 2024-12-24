fd0f746380745c18907e577a37f545bff2819133
Daniel Muir <64096716+Zeckten@users.noreply.github.com>
2024-12-24 15:48:16 -0800

    password string to text

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.Text, nullable=False)
     nylas_grant_id = db.Column(db.String(255), unique=True, nullable=True)
 
     def __repr__(self):
